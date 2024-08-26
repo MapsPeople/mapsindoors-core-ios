54c7efa43831696abacf56440044040641556fc8
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-08-26 14:48:38 +0200

    Release 4.6.0-beta.18

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let mapsindoorsVersion = Version("4.6.0-beta.18")
+let checksum = "1e0380fa5c497814b08be4bee585b6feac567321a1feba89b4e9041f56eeb94f"
+
+let package = Package(
+    name: "MapsIndoorsCore",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(name: "MapsIndoorsCore", targets: ["MapsIndoorsCoreWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MapsPeople/mapsindoors-ios.git", exact: mapsindoorsVersion),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MapsIndoorsCore",
+            url: "https://github.com/MapsPeople/MapsIndoors-SDK-iOS/releases/download/\(mapsindoorsVersion)/MapsIndoorsCore.xcframework.zip",
+            checksum: checksum
+        ),
+        .target(
+            name: "MapsIndoorsCoreWrapper",
+            dependencies: [
+                "MapsIndoorsCore",
+                .product(name: "MapsIndoors", package: "mapsindoors-ios"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/MapsIndoorsCoreWrapper/MPEmpty.swift
@@ -0,0 +1,9 @@
+//
+//  MPEmpty.swift
+//
+//
+//  Created by Christian Wolf Johannsen on 12/02/2024.
+//
+
+import Foundation
+import MapsIndoors
