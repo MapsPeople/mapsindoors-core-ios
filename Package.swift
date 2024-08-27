33e3fcd7326b9937a96f95bbb3a542fd7a5f5432
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-08-27 10:54:51 +0200

    Rename stuff

--- Package.swift
+++ Package.swift
@@ -9,21 +9,21 @@ let package = Package(
     name: "MapsIndoorsCore",
     platforms: [.iOS(.v14)],
     products: [
-        .library(name: "MapsIndoorsCore", targets: ["MapsIndoorsCore"]),
+        .library(name: "MapsIndoorsCore", targets: ["MapsIndoorsCoreWrapper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/MapsPeople/mapsindoors-ios.git", exact: mapsindoorsVersion),
     ],
     targets: [
         .binaryTarget(
-            name: "MapsIndoorsCoreXCFramework",
+            name: "MapsIndoorsCore",
             url: "https://github.com/MapsPeople/MapsIndoors-SDK-iOS/releases/download/\(mapsindoorsVersion)/MapsIndoorsCore.xcframework.zip",
             checksum: "f59a17614d4408dc78be03073cd74e21d700dd5b7de67c4e1c1f603900df9152"
         ),
         .target(
-            name: "MapsIndoorsCore",
+            name: "MapsIndoorsCoreWrapper",
             dependencies: [
-                "MapsIndoorsCoreXCFramework",
+                "MapsIndoorsCore",
                 .product(name: "MapsIndoors", package: "mapsindoors-ios"),
             ]
         ),
