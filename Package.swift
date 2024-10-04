29a4222bd8cadc3883978a80c3ab557b95a48001
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-10-04 12:51:08 +0200

    Release version 4.6.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.6.0-beta.18")
-let checksum = "1e0380fa5c497814b08be4bee585b6feac567321a1feba89b4e9041f56eeb94f"
+let mapsindoorsVersion = Version("4.6.0")
+let checksum = "87bbe6f44fee9d00d125252c0f43fd58d54c9d6002984355d2e0adee2150cf9c"
 
 let package = Package(
     name: "MapsIndoorsCore",
